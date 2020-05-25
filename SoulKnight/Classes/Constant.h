b07b498e6e7426d187df72fdfecff83d131bc879
Shankfizz <1250103947@qq.com>
2020-05-25 22:29:27 +0800

    先写了一些类，之后继续完善

---
+++ SoulKnight/Classes/Constant.h
@@ -0,0 +1,49 @@
+#pragma once
+#ifndef __CONSTANT_H_
+#define __CONSTANT_H_
+
+#include"cocos2d.h"
+
+#define INIT_HITPOINTS 5			//初始化生命值
+#define INIT_SHIELD 5				//初始化护盾值
+#define INIT_MANAPOINTS 180			//初始化法力值
+
+#define SHIELD_RECOVER_TIME 1.0f     //主角护盾击破后刷新时间初始化1.0f
+#define INIT_ID_RADIUS 500        //移动角色的感应半径，探知范围内是否有其他移动物体         
+/* 
+*没有具体测量，暂时设置500  
+*/
+#define INIT_MOVESPEED 1.0
+
+enum EAttackMode	//	攻击模式,可以是武器的攻击模式，也可是怪物的攻击模式
+{
+	MELEE,			//	近战
+	REMOTE,			//	远程
+	MIX				//  混合
+};
+enum DamageMode      //伤害类型	
+{
+	//瞬时伤害
+	//持续伤害
+};
+
+enum AllCamp        //阵营
+{
+	FRIENDLY,
+	ENEMY
+};
+//可能会定义的
+
+//可能会定义的方向枚举类型
+//enum EDirection
+
+
+
+//可能会
+
+
+
+
+
+
+#endif //!__CONSTANT_H_
---
+++ SoulKnight/Classes/MovingActor.h
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef __MOVINGACTOR_H__
+#define __MOVINGACTOR_H__
+#include<iostream>
+#include <string>
+#include <time.h>
+#include "cocos2d.h"
+#include"Constant.h"
+
+USING_NS_CC;
+
+class HelloWorld;
+
+class MovingActor :public cocos2d::Sprite
+{
+	CC_SYNTHESIZE(AllCamp, camp, Camp);//阵营
+	CC_SYNTHESIZE(INT32, hitPoints, Hitpoints);   //生命值
+	CC_SYNTHESIZE(float, identityRadius, IdentityRadius); //识别半径用于感知范围
+	CC_SYNTHESIZE(float, moveSpeed, MoveSpeed); //移动速度
+	CC_SYNTHESIZE(float, attackSpeed, AttackSpeed); //攻击速度/攻击频次
+	CC_SYNTHESIZE(bool, alreadyDead, AlreadyDead); //死亡判定
+	CC_SYNTHESIZE(HelloWorld*, exploreScene, ExploreScene);   //加入场景
+	CC_SYNTHESIZE(MovingActor*,attackFrom, AttackFrom);//伤害来源
+	CC_SYNTHESIZE(MovingActor*, attackTarget, AttackTarget);//攻击目标
+
+protected:
+
+	virtual void initData(HelloWorld* Scene);  //防止空定义
+
+	//virtual void removeBuff(Buff* buff);
+public:
+
+	virtual bool init(const std::string& filename, HelloWorld* Scene);
+	virtual void updateTarget();
+	virtual void die();
+	virtual void takeDamage(DamageMode type, INT32 damage, MovingActor* enemy);
+	static MovingActor* create(const std::string& filename, HelloWorld* Scene);
+};
+
+
+
+
+
+#endif // !__MOVINGACTOR_H__
