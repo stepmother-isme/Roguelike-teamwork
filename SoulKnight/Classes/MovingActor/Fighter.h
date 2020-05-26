8d612a289e07bcfd0869827c66835f85484cda4a
lyf-1953598 <realsad@126.com>
2020-05-26 12:39:45 +0800

    更新了Fighter.h/cpp,添加了plist文件

--- SoulKnight/Classes/MovingActor/Fighter.h
+++ SoulKnight/Classes/MovingActor/Fighter.h
@@ -1,30 +1,73 @@
 #pragma once
 #ifndef __FIGHTER_H__
 #define __FIGHTER_H__
-
-#include"cocos2d.h"
-#include"MovingActor.h"
+#include "cocos2d.h"
+#include "MovingActor.h"
 
 USING_NS_CC;
 
+class Equipment;
+
 class Fighter :public MovingActor
 {
-	CC_SYNTHESIZE(INT32, magicPoint, MagicPoint);//能量
-	CC_SYNTHESIZE(INT32, shield, Shield);//护盾
-	CC_SYNTHESIZE(INT32, shieldFix, ShieldFix);//护盾的回复速度
-	CC_SYNTHESIZE(String, fighterName, FighterName);//英雄名字
-	CC_SYNTHESIZE(float, calmTime, CalmTime);//技能冷却时间
-	CC_SYNTHESIZE(float, lastTime, LastTime);//技能持续时间
+	CC_SYNTHESIZE(EAttackMode, attackMode, AttackMode);//设置攻击模式的切换 近战/远程
+
+	CC_SYNTHESIZE(int, shield, Shield);                //护甲值上限
+	CC_SYNTHESIZE(int, curShield, CurShield);		   //当前护甲值
+	CC_SYNTHESIZE(float, acRcoverSpeed, ACRcoverSpeed); //护甲刷新时间
+	CC_SYNTHESIZE(float, lastTimeInjured, LastTimeInjured); //最后受到伤害的时间，用于刷新护盾
+
+	CC_SYNTHESIZE(int, manaPoints, ManaPoints);			//法力值上限
+	CC_SYNTHESIZE(int, curManaPoints, CurManaPoints);	//当前法力值
+
+	CC_SYNTHESIZE(float, critRate, CritRate);              //暴击率
+
+	CC_SYNTHESIZE(String, fighterName, FighterName);	//获取角色名
+	CC_SYNTHESIZE(int, equipNumber, EquipNumber);     //所能携带的武器数量
+
+	
+
 public:
-	//virtual void getEquipment(Equipment* Eq);  //获取武器
-	//virtual void exchangeEquipment(Equipment* Eq) //交换武器
-	virtual void takeDamage(DamageMode type, INT32 damage, MovingActor* enemy);
-	virtual void skill();
-	virtual void reborn();
-	virtual void move();
-	virtual void stopMove();
-	virtual void movingAnimation();
-	virtual void attackAnimation();
+
+	virtual bool isFullEquipments();               //判断是否带满武器
+
+	//Equipment* getEquipByNumber(int equipNumber);  //获得武器
+
+	Equipment* changeMainEquip();  //切换主武器
+
+	virtual bool attack();						  //是否攻击
+
+	//virtual void takeBuff(Buff* buff);			//获得buff  TBD
+
+	virtual void releaseSkill();               //发起技能
+
+	virtual void fighterMove();               //发起移动
+
+	virtual bool isInMelee();                  //是否在近战范围
+
+	virtual bool init(HelloWorld* Scene, std::string fighterName);
+
+	static Fighter* create(HelloWorld* Scene, std::string fighterName);   //Unknown
+
+	virtual void playAttackAnimation();   //？？？ Unknown
+
+protected:
+
+
+	ValueMap initFighterData;
+
+	Equipment* equips[INIT_EQUIP_NUMBER];  //武器栏初始化
+
+	virtual void die();                  //角色死亡时进行的操作
+
+	//virtual void removeAllBuff();			
+
+	//virtual void removeBuff(Buff* buff);
+
+	//virtual void updateDirection();
+	//※※此处参照学长所做修正方向的函数，如有需要待定义，待编写
+
+	virtual bool initHeroData(HelloWorld* Scene, std::string fighterName);
 };
 
 
--- SoulKnight/Classes/MovingActor/MovingActor.cpp
+++ SoulKnight/Classes/MovingActor/MovingActor.cpp
@@ -5,7 +5,7 @@
 
 MovingActor* MovingActor::create(const std::string& filename, HelloWorld* Scene)
 {
-	MovingActor* sprite = new MovingActor();
+	MovingActor* sprite = new  MovingActor();
 	if (sprite && sprite->init(filename, Scene))
 	{
 		sprite->autorelease();
@@ -33,9 +33,10 @@ void MovingActor::initData(HelloWorld* Scene)
 {
 	exploreScene = Scene;
 	hitPoints = INIT_HITPOINTS;      //初始生命值5
+	curHitPoints = hitPoints;       //当前生命值为满值
 	identityRadius = INIT_ID_RADIUS;//初始感知半径500，boss可能会更大
 	moveSpeed = INIT_MOVESPEED; 
-	attackSpeed = 0;           //主角和enemy攻击机制不同，暂时设为0
+	attackSpeed = 0.f;           //主角和enemy攻击机制不同，暂时设为0
 	alreadyDead = false;
 
 
