39b942b09db088d1f36108ee8ba8a31c9a18df78
stepmother-isme <niannianwwj@163.com>
2020-05-26 08:35:05 +0800

    增加GameScene游戏场景

--- SoulKnight/Classes/HelloWorldScene.cpp
+++ SoulKnight/Classes/HelloWorldScene.cpp
@@ -1,18 +1,18 @@
 /****************************************************************************
  Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,30 +27,31 @@
 #include "Knight.h"
 
 
+
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
 {
-    return HelloWorld::create();
+	return HelloWorld::create();
 }
 
 static void problemLoading(const char* filename)
 {
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
 }
 
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Scene::init() )
-    {
-        return false;
-    }
+	//////////////////////////////
+	// 1. super init first
+	if (!Scene::init())
+	{
+		return false;
+	}
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 	//以下创建动画的代码参考自：https://blog.csdn.net/congcongjoy/article/details/8753452
 	//有改动
@@ -63,7 +64,7 @@ bool HelloWorld::init()
 	CCSpriteFrame* frameOriginal = NULL;
 
 	CCTexture2D* playerRight = CCTextureCache::sharedTextureCache()->addImage("rightDir.png");
-	frame = CCSpriteFrame::createWithTexture(playerRight, CCRectMake(0,0,30,60));
+	frame = CCSpriteFrame::createWithTexture(playerRight, CCRectMake(0, 0, 30, 60));
 	framesRight.pushBack(frame);
 
 	CCTexture2D* playerLeft = CCTextureCache::sharedTextureCache()->addImage("leftDir.png");
@@ -78,8 +79,6 @@ bool HelloWorld::init()
 	CCTexture2D* playerUp = CCTextureCache::sharedTextureCache()->addImage("upDir.png");
 	frame = CCSpriteFrame::createWithTexture(playerUp, CCRectMake(0, 0, 30, 60));
 	framesUp.pushBack(frame);
-
-
 
 	//通过数组创建帧序列
 	CCAnimation* animitionUp = CCAnimation::createWithSpriteFrames(framesUp, 0.1f);
@@ -98,13 +97,21 @@ bool HelloWorld::init()
 	actionLeft->retain();
 	actionRight->retain();
 
+
+	//以下需要改变位置
+
+	//创建背景
+	auto bg = Sprite::create("securityMap.png"); 
+	bg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	addChild(bg, 0);
+
 	//创建角色
 	auto hero = Sprite::createWithSpriteFrame(frameOriginal);
-	hero->setPosition(Vec2(visibleSize.width / 2, 70));
+	hero->setPosition(Vec2(visibleSize.width / 2, 240));
 	this->addChild(hero);
 
 
-	//键盘事件的监听器，有什么想法可以改改，目前HelloWorld水平
+	//键盘事件的监听器，有什么想法可以改改，目前HelloWorld水平——使用lambda表达式处理键盘事件
 	auto Listener = EventListenerKeyboard::create();
 	Listener->onKeyPressed = [=](EventKeyboard::KeyCode keycode, Event* event)
 	{
@@ -119,7 +126,7 @@ bool HelloWorld::init()
 		break;
 		case EventKeyboard::KeyCode::KEY_D:
 		{
-			auto actionRight = Spawn::createWithTwoActions(CCAnimate::create(animitionRight), MoveBy::create(0.4f, Vec2(50,0)));
+			auto actionRight = Spawn::createWithTwoActions(CCAnimate::create(animitionRight), MoveBy::create(0.4f, Vec2(50, 0)));
 			actionRight->retain();
 			hero->runAction(actionRight);
 		}
@@ -133,7 +140,7 @@ bool HelloWorld::init()
 		break;
 		case EventKeyboard::KeyCode::KEY_S:
 		{
-			auto actionDown = Spawn::createWithTwoActions(CCAnimate::create(animitionDown), MoveBy::create(0.4f, Vec2(0,-50)));
+			auto actionDown = Spawn::createWithTwoActions(CCAnimate::create(animitionDown), MoveBy::create(0.4f, Vec2(0, -50)));
 			actionDown->retain();
 			hero->runAction(actionDown);
 		}
@@ -146,10 +153,11 @@ bool HelloWorld::init()
 	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(Listener, this);
 
 
-    return true;
+	return true;
 }
+
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-    Director::getInstance()->end();
+	Director::getInstance()->end();
 }
