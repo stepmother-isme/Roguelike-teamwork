6a091d327052965dca73d74d7e0bcd49ff61968c
stepmother-isme <niannianwwj@163.com>
2020-06-06 16:30:39 +0800

    上一次误操作补救

--- SoulKnight/Classes/AppDelegate.cpp
+++ SoulKnight/Classes/AppDelegate.cpp
@@ -1,8 +1,11 @@
 #include "AppDelegate.h"
-//#include "Scene/StartGameScene.h"
-#include "Scene/GameScene.h"
-// #define USE_AUDIO_ENGINE 1
-// #define USE_SIMPLE_AUDIO_ENGINE 1
+#include "Scene/StartGameScene.h"
+#include "SimpleAudioEngine.h" 
+
+
+
+ // #define USE_AUDIO_ENGINE 1
+ // #define USE_SIMPLE_AUDIO_ENGINE 1
 
 #if USE_AUDIO_ENGINE && USE_SIMPLE_AUDIO_ENGINE
 #error "Don't use AudioEngine and SimpleAudioEngine at the same time. Please just select one in your game!"
@@ -27,102 +30,111 @@ AppDelegate::AppDelegate()
 {
 }
 
-AppDelegate::~AppDelegate() 
+AppDelegate::~AppDelegate()
 {
 #if USE_AUDIO_ENGINE
-    AudioEngine::end();
+	AudioEngine::end();
 #elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::end();
+	SimpleAudioEngine::end();
 #endif
 }
 
-
+// if you want a different context, modify the value of glContextAttrs
+// it will affect all platforms
 void AppDelegate::initGLContextAttrs()
 {
+	// set OpenGL context attributes: red,green,blue,alpha,depth,stencil,multisamplesCount
+	GLContextAttrs glContextAttrs = { 8, 8, 8, 8, 24, 8, 0 };
 
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8, 0};
-
-    GLView::setGLContextAttrs(glContextAttrs);
+	GLView::setGLContextAttrs(glContextAttrs);
 }
 
 // if you want to use the package manager to install more packages,  
 // don't modify or remove this function
 static int register_all_packages()
 {
-    return 0; //flag for packages manager
+	return 0; //flag for packages manager
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
-    auto director = Director::getInstance();
-    auto glview = director->getOpenGLView();
-    if(!glview) {
+	// initialize director
+	auto director = Director::getInstance();
+	auto glview = director->getOpenGLView();
+	if (!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("SoulKnight", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+		glview = GLViewImpl::createWithRect("SoulKnight", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glview = GLViewImpl::create("SoulKnight");
+		glview = GLViewImpl::create("SoulKnight");
 #endif
-        director->setOpenGLView(glview);
-    }
+		director->setOpenGLView(glview);
+	}
 
-    // turn on display FPS
-    director->setDisplayStats(true);
+	// turn on display FPS
+	director->setDisplayStats(true);
 
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0f / 60);
+	// set FPS. the default value is 1.0/60 if you don't call this
+	director->setAnimationInterval(1.0f / 60);
 
-    // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
-    auto frameSize = glview->getFrameSize();
-    // if the frame's height is larger than the height of medium size.
-    if (frameSize.height > mediumResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is larger than the height of small size.
-    else if (frameSize.height > smallResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is smaller than the height of medium size.
-    else
-    {        
-        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
-    }
+	// Set the design resolution
+	glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+	auto frameSize = glview->getFrameSize();
+	// if the frame's height is larger than the height of medium size.
+	if (frameSize.height > mediumResolutionSize.height)
+	{
+		director->setContentScaleFactor(MIN(largeResolutionSize.height / designResolutionSize.height, largeResolutionSize.width / designResolutionSize.width));
+	}
+	// if the frame's height is larger than the height of small size.
+	else if (frameSize.height > smallResolutionSize.height)
+	{
+		director->setContentScaleFactor(MIN(mediumResolutionSize.height / designResolutionSize.height, mediumResolutionSize.width / designResolutionSize.width));
+	}
+	// if the frame's height is smaller than the height of medium size.
+	else
+	{
+		director->setContentScaleFactor(MIN(smallResolutionSize.height / designResolutionSize.height, smallResolutionSize.width / designResolutionSize.width));
+	}
 
-    register_all_packages();
+	register_all_packages();
 
- 
-    //auto scene = StartGame::createScene();
-	auto scene = GameScene::createScene();
-    // run
-    director->runWithScene(scene);
+	// create a scene. it's an autorelease object
+	auto scene = StartGame::createScene();
 
-	//CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic("ArtDesigning/Audio/StartGame.mp3");
-	//CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic("ArtDesigning/Audio/SafetyMap.mp3");
-    return true;
+	// run
+	director->runWithScene(scene);
+	
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic("ArtDesigning/Audio/StartGame.mp3");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic("ArtDesigning/Audio/SafetyMap.mp3");
+    
+	//原文链接：https://aladdin.blog.csdn.net/article/details/11894879	
+	//深度测试，方便实现遮盖效果
+	CCDirector::sharedDirector()->setDepthTest(true);
+	//Opengl渲染设置,如果地图有背景图层的话就需要加这句
+	CCDirector::sharedDirector()->setProjection(kCCDirectorProjection2D);
+		
+
+	return true;
 }
 
 // This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
 void AppDelegate::applicationDidEnterBackground() {
-    Director::getInstance()->stopAnimation();
+	Director::getInstance()->stopAnimation();
 
 #if USE_AUDIO_ENGINE
-    AudioEngine::pauseAll();
+	AudioEngine::pauseAll();
 #elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
-    SimpleAudioEngine::getInstance()->pauseAllEffects();
+	SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+	SimpleAudioEngine::getInstance()->pauseAllEffects();
 #endif
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
-    Director::getInstance()->startAnimation();
+	Director::getInstance()->startAnimation();
 
 #if USE_AUDIO_ENGINE
-    AudioEngine::resumeAll();
+	AudioEngine::resumeAll();
 #elif USE_SIMPLE_AUDIO_ENGINE
-    SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
-    SimpleAudioEngine::getInstance()->resumeAllEffects();
+	SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+	SimpleAudioEngine::getInstance()->resumeAllEffects();
 #endif
 }
