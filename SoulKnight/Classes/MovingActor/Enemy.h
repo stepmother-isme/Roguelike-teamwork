14df8067e0d02edbbb980222d4fb60d25b4bc2f1
stepmother-isme <65219650+stepmother-isme@users.noreply.github.com>
2020-06-14 21:18:41 +0800

    Update Enemy.h

--- SoulKnight/Classes/MovingActor/Enemy.h
+++ SoulKnight/Classes/MovingActor/Enemy.h
@@ -3,19 +3,21 @@
 #define __ENEMY_H__
 
 #include"cocos2d.h"
-#include"MovingActor/Constant.h"
 #include"MovingActor/MovingActor.h"
 
 USING_NS_CC;
 
 class Enemy :public MovingActor
 {
+	CC_SYNTHESIZE(bool,isToMove, IsToMove)
+	CC_SYNTHESIZE(bool,isMoving, IsMoving);
 	CC_SYNTHESIZE(String, enemyName, EnemyName);
-	CC_SYNTHESIZE(EnemyLevel, level, Level);
 	CC_SYNTHESIZE(Vec2, destination, Destination);
 	CC_SYNTHESIZE(float, attackRadius, AttackRadius);
 	CC_SYNTHESIZE(MovingActor*, fromFighter, FromFighter);
 	CC_SYNTHESIZE(EDirection, fDirecition, FDirection);
+
+	CCSprite* m_sprite;
 
 public:
 	static Enemy* create(GameScene* Scene, std::string fighterName);
@@ -23,8 +25,8 @@ public:
 	bool initData(GameScene* Scene, std::string fighterName);
 	bool attack();
 
-
-
+	void bindSprite(CCSprite* sprite);
+	void updateAction();
 	void updateDestination();
 	void updateTarget();
 	void enemyMove();
